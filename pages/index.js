bb2bba8e116dd8c30fed053982f79e85ee2f5053
Justin Coates <Jcoates1997@hotmail.com>
2022-04-21 13:11:50 +1000

    service workers

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,42 @@
-import Home from '../components/home/home'
-import Link from "next/link"
-import { useSession, getSession  } from "next-auth/react"
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
+import Home from "../components/home/home";
+import Link from "next/link";
+import { useSession, getSession } from "next-auth/react";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 function HomePage({ ip }) {
-
     const router = useRouter();
-    const { data: session, status } = useSession()
+    const { data: session, status } = useSession();
 
     //If not logged in redirects to the signin page
-    useEffect(() => {
-        const securePage = async() => {
-            const session = await getSession()
-            if(!session && !status) {
-                return router.push('/signin')
-            } else {
-                console.log(session)
-            }
-        }
-        securePage()
-    }, [])
-    
+    // useEffect(() => {
+    //     const securePage = async () => {
+    //         const session = await getSession();
+    //         if (!session && !status) {
+    //             return router.push("/signin");
+    //         } else {
+    //             console.log(session);
+    //         }
+    //     };
+    //     securePage();
+    // }, []);
+
     return (
         <>
-        {   session &&
+            {/* {   session &&
             <div>singed in as {session.user.email} ip: {ip}
             <button><Link href="/admin">admin page</Link></button></div>
-        }
+        } */}
             {/* <pre>{JSON.stringify(users, null, 4)}</pre> */}
             <Home />
         </>
-    )
+    );
 }
 export default HomePage;
 
-
-export async function getServerSideProps({ req }) {
-    const forwarded = req.headers["x-forwarded-for"]
-    const ip = forwarded ? forwarded.split(/, /)[0] : req.connection.remoteAddress
-    console.log(ip)
-    return {props: {ip}}
-}
-  
+// export async function getServerSideProps({ req }) {
+//     const forwarded = req.headers["x-forwarded-for"]
+//     const ip = forwarded ? forwarded.split(/, /)[0] : req.connection.remoteAddress
+//     console.log(ip)
+//     return {props: {ip}}
+// }
