bee7d4873ef2a4b94751914e85b587ac6efb720e
Justin Coates <Jcoates1997@hotmail.com>
2022-06-11 21:16:55 +1000

    prisma changes

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,31 +1,14 @@
-// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
-// let prisma: PrismaClient;
+let prisma: PrismaClient;
 
-// if (process.env.NODE_ENV === "production") {
-//     prisma = new PrismaClient();
-// } else {
-//     if (!global.prisma) {
-//         global.prisma = new PrismaClient();
-//     }
-//     prisma = global.prisma;
-// }
-
-// export default prisma;
-
-
-import { PrismaClient } from '@prisma/client'
-
-declare global {
-  // allow global `var` declarations
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined
+if (process.env.NODE_ENV === "production") {
+    prisma = new PrismaClient();
+} else {
+    if (!global.prisma) {
+        global.prisma = new PrismaClient();
+    }
+    prisma = global.prisma;
 }
 
-export default prisma =
-  global.prisma ||
-  new PrismaClient({
-    log: ['query'],
-  })
-
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma
+export default prisma;
